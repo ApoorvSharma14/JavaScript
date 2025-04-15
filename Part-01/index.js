26551836be2ecb8c52c3eb0da3f76a83dde56cd2
ApoorvSharma14 <apoorv03sharma@gmail.com>
2025-04-16 00:59:28 +0530

    index.js changes

---
+++ Part-01/index.js
@@ -0,0 +1,81 @@
+//VARIABLES/////////////////
+
+// alert("Hello World!");
+// let a = 10;
+// console.log(a);
+// const PI = 3.14159265358979323846;
+
+
+
+//DATA TYPES//////////////////////
+
+//Number Data Type////////////////////
+
+/*let num1 = 5; // Number
+console.log(typeof num1); // Number
+
+let num2 = 10.012; // Number
+console.log(typeof num2); // Number
+
+let num3 = Math.ceil(num2);
+console.log(num3); // 11
+
+let num4 = Math.floor(num2);
+console.log(num4); // 10
+
+
+//String Data Type///////////////////
+
+let str1 = "Hello World!"; // String
+console.log(str1,typeof str1); // Hello World! String
+
+let str2 = 'a'; // String
+console.log(str2,typeof str2); // String
+
+let num5 = str2.charCodeAt(0); // Convert string to number
+console.log(num5); // 97
+
+let str3 = "100";
+console.log(typeof str3); // String
+
+let num6 = Number(str3); // Convert string to number
+console.log(num6,typeof num6); // 100 Number
+
+
+//Boolean Data Type////////////////////
+let bool1 = true; // Boolean
+console.log(bool1,typeof bool1); // true Boolean
+
+
+let num7 = Boolean(0);
+console.log(num7); // false
+
+let str4 = Boolean("Apoorv"); // Convert string to boolean
+console.log(str4,typeof str4); // true
+
+
+//////Undefined Data Type////////////////////
+let und1; // Undefined
+console.log(und1,typeof und1); // undefined
+*/
+
+///BigInt Data Type///////////////////////
+
+
+/* const num = 100000000000000101010;
+console.log(BigInt(num)); // 100000000000000101010n
+
+const num1 = BigInt(1);
+console.log(num1); // 1n
+
+const num2 = BigInt(10.63636363)
+console.log(num2); //Cannot covert floating point literal to BigInt*/
+
+/*const firstName = 'Apoorv';
+const lastName = 'Sharma';
+console.log(firstName + lastName); // ApoorvSharma
+console.log(firstName + " " + lastName); // Apoorv Sharma
+
+const age = 22;
+
+console.log(`My name is ${firstName} ${lastName} and I am ${age} years old.`); // My name is Apoorv Sharma and I am 22 years old.*/
